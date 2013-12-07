05b37d133c6bbbdb7caa074e69f9c55584d73e7d
Jonathan Webb <webbj2012@jwwebb.info>
2013-12-07 13:51:07 -0500

    Renames Json test file. Imports test array from file.

---
+++ test/Acquia/Test/Common/.JsonTestArray.php
@@ -0,0 +1,35 @@
+array(
+    "foo" => array(
+        "bar" => "bar foo",
+        "Food" => array(
+            "bar" => "X",
+            "Fool" => array(
+                "Foolery" => array(
+                    "ID" => "LAMA",
+                    "SortOf" => "LAMA",
+                    "Foot" => "Lorem Aliquam Morbi Aenean",
+                    "Aenean" => "LAMA",
+                    "Aliquam" => "ABC 1234:5678",
+                    "Foodie" => array(
+                        "para" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        "FoosBall" => array(
+                            "ABC",
+                            "123"
+                        ),
+                    ),
+                    "Footsie" => "quisquam"
+                )
+            )
+        )
+    ),
+    'test' => array(
+        '<foo>',
+        "'bar'",
+        '"baz"',
+        '&blong&',
+        "\xc3\xa9",
+        '/path/to/api.json',
+        "/another\\/path\\//to/api.json",
+        '\Acquia\Common\Json',
+    )
+)
--- test/Acquia/Test/Common/JsonTest.php
+++ test/Acquia/Test/Common/JsonTest.php
@@ -8,46 +8,13 @@ use Acquia\Common\Json;
 class JsonTest extends \PHPUnit_Framework_TestCase
 {
     protected function getTestJson() {
-        $test_file = dirname(__FILE__) . "/.test.json";
+        $test_file = dirname(__FILE__) . "/.JsonTestArray.json";
         return file_get_contents($test_file);
     }
 
     protected function getTestArray() {
-        return array(
-            "foo" => array(
-                "bar" => "bar foo",
-                "Food" => array(
-                    "bar" => "X",
-                    "Fool" => array(
-                        "Foolery" => array(
-                            "ID" => "LAMA",
-                            "SortOf" => "LAMA",
-                            "Foot" => "Lorem Aliquam Morbi Aenean",
-                            "Aenean" => "LAMA",
-                            "Aliquam" => "ABC 1234:5678",
-                            "Foodie" => array(
-                                "para" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                                "FoosBall" => array(
-                                    "ABC",
-                                    "123"
-                                ),
-                            ),
-                            "Footsie" => "quisquam"
-                        )
-                    )
-                )
-            ),
-            'test' => array(
-                '<foo>',
-                "'bar'",
-                '"baz"',
-                '&blong&',
-                "\xc3\xa9",
-                '/path/to/api.json',
-                "/another\\/path\\//to/api.json",
-                '\Acquia\Common\Json',
-            )
-        );
+        $test_file = dirname(__FILE__) . "/.JsonTestArray.php";
+        return eval('return ' . file_get_contents($test_file) . ';');
     }
 
     public function testJsonEncode()
