97aeda2bff30372d5544674de0a57adca9c41cc0
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2013-12-17 14:54:11 -0500

    Removed remaining references to cpliakas, changed version.

--- src/Acquia/Network/AcquiaNetworkClient.php
+++ src/Acquia/Network/AcquiaNetworkClient.php
@@ -11,7 +11,7 @@ use Guzzle\Service\Client;
 class AcquiaNetworkClient extends Client implements AcquiaServiceManagerAware
 {
     const NONCE_LENGTH = 55;
-    const VERSION = '0.3';
+    const VERSION = '0.4';
 
     /**
      * @var string
@@ -270,7 +270,7 @@ class AcquiaNetworkClient extends Client implements AcquiaServiceManagerAware
      */
     public function checkSubscription()
     {
-        $options += array('cpliakas/acquia-search-sdk' => self::VERSION);
+        $options += array('acquia/acquia-search-sdk' => self::VERSION);
         return $this->getSubscription($options);
     }
 
